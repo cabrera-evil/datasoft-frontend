a4fdd3578bece24b2fa77e59e2b1b44f9e105f43
cabrera-evil <douglascabrera.dev@gmail.com>
2023-11-25 15:02:14 -0600

    Feat: get products form api

--- src/pages/Feed/Feed.tsx
+++ src/pages/Feed/Feed.tsx
@@ -1,29 +1,28 @@
+import { useEffect, useState } from "react";
 import FeedItem from "./components/FeedItem/FeedItem";
+import { IProductModel } from '../../models/IProductModel.interface';
+import ProductService from "../../services/Product.service";
 
 export default function Feed() {
-  // Supongamos que tienes un array de datos que representan tus elementos.
-  const feedItemsData = [
-    { imageSrc: "https://user-images.githubusercontent.com/2805249/64069899-8bdaa180-cc97-11e9-9b19-1a9e1a254c18.png", category: "Indoor", name: "Peace Lily", price: 36.00 },
-    { imageSrc: "https://user-images.githubusercontent.com/2805249/64069899-8bdaa180-cc97-11e9-9b19-1a9e1a254c18.png", category: "Indoor", name: "Peace Lily", price: 36.00 },
-    { imageSrc: "https://user-images.githubusercontent.com/2805249/64069899-8bdaa180-cc97-11e9-9b19-1a9e1a254c18.png", category: "Indoor", name: "Peace Lily", price: 36.00 },
-    { imageSrc: "https://user-images.githubusercontent.com/2805249/64069899-8bdaa180-cc97-11e9-9b19-1a9e1a254c18.png", category: "Indoor", name: "Peace Lily", price: 36.00 },
-    { imageSrc: "https://user-images.githubusercontent.com/2805249/64069899-8bdaa180-cc97-11e9-9b19-1a9e1a254c18.png", category: "Indoor", name: "Peace Lily", price: 36.00 },
-    { imageSrc: "https://user-images.githubusercontent.com/2805249/64069899-8bdaa180-cc97-11e9-9b19-1a9e1a254c18.png", category: "Indoor", name: "Peace Lily", price: 36.00 },
-    { imageSrc: "https://user-images.githubusercontent.com/2805249/64069899-8bdaa180-cc97-11e9-9b19-1a9e1a254c18.png", category: "Indoor", name: "Peace Lily", price: 36.00 },
-    { imageSrc: "https://user-images.githubusercontent.com/2805249/64069899-8bdaa180-cc97-11e9-9b19-1a9e1a254c18.png", category: "Indoor", name: "Peace Lily", price: 36.00 },
-    { imageSrc: "https://user-images.githubusercontent.com/2805249/64069899-8bdaa180-cc97-11e9-9b19-1a9e1a254c18.png", category: "Indoor", name: "Peace Lily", price: 36.00 },
-    { imageSrc: "https://user-images.githubusercontent.com/2805249/64069899-8bdaa180-cc97-11e9-9b19-1a9e1a254c18.png", category: "Indoor", name: "Peace Lily", price: 36.00 },
-  ];
+  const [feedItemsData, setFeedItemsData] = useState<IProductModel[]>([]);
+  const productService = new ProductService();
+
+  useEffect(() => {
+    productService.getProduct()
+      .then((data) => {
+        setFeedItemsData(data);
+      });
+  }, []);
 
   return (
     <div className="flex flex-wrap gap-4">
       {feedItemsData.map((item, index) => (
         <FeedItem
           key={index}
-          imageSrc={item.imageSrc}
-          category={item.category}
-          name={item.name}
-          price={item.price}
+          imageSrc={item.prdImage}
+          category={item.categoryModel.catName}
+          name={item.prdName}
+          price={item.prdPrice}
         />
       ))}
     </div>
--- src/pages/Feed/components/FeedItem/FeedItem.tsx
+++ src/pages/Feed/components/FeedItem/FeedItem.tsx
@@ -7,7 +7,7 @@ interface FeedItemProps {
     price: number;
 }
 
-export default function FeedItem({ imageSrc, category, name, price }:   FeedItemProps) {
+export default function FeedItem({ imageSrc, category, name, price }: FeedItemProps) {
     return (
         <div className={`flex-shrink-0 m-6 relative overflow-hidden rounded-lg max-w-xs shadow-lg`}>
             <svg className="absolute bottom-0 left-0 mb-8" viewBox="0 0 375 283" fill="none" style={{ transform: 'scale(1.5)', opacity: 0.1 }}>
@@ -16,7 +16,7 @@ export default function FeedItem({ imageSrc, category, name, price }:   FeedItem
             </svg>
             <div className="relative pt-10 px-10 flex items-center justify-center">
                 <div className="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3" style={{ background: 'radial-gradient(black, transparent 60%)', transform: 'rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1)', opacity: 0.2 }}></div>
-                <img className="relative w-40" src={imageSrc} alt="" />
+                <img className="relative w-40 h-40" src={imageSrc} alt="" style={{ objectFit: 'contain' }} />
             </div>
             <div className={`relative text-black px-6 pb-6 mt-6`}>
                 <span className="block opacity-75 -mb-1">{category}</span>
